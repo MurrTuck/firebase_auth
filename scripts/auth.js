1f8687e230e0d8567040b40aecd5e6b6b6032ea5
MurrTuck <murrtuck@gmail.com>
2019-01-09 17:36:46 -0700

    Logout Method

--- scripts/auth.js
+++ scripts/auth.js
@@ -9,10 +9,20 @@ signupForm.addEventListener('submit', (e) => {
 
     // sign up the user
     auth.createUserWithEmailAndPassword(email, password).then(cred => {
-        console.log(cred.user);
         // close the signup modal & reset form
         const modal = document.querySelector('#modal-signup');
         M.Modal.getInstance(modal).close();
         signupForm.reset();
     });
+});
+
+//logout
+const logout = document.querySelector('#logout');
+logout.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    auth.signOut().then(() => {
+        // Don't work in the dark!
+        console.log('user is signed out')
+    })
 });
