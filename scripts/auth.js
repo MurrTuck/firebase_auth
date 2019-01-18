9089ab077db708491e2de40bee7925a530df02d4
MurrTuck <murrtuck@gmail.com>
2019-01-18 15:29:43 -0700

    Updating Database with Realtime Listener

--- scripts/auth.js
+++ scripts/auth.js
@@ -2,7 +2,7 @@
 //Listen for auth changes
 auth.onAuthStateChanged(user => {
     if (user) {
-        db.collection('guides').get().then(snapshot => {
+        db.collection('guides').onSnapshot(snapshot => {
             setupGuides(snapshot.docs)
             setupUI(user);
         })
