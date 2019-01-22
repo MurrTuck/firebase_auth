f7212a5965a7d92077082552ec5c6321f86c3b95
MurrTuck <murrtuck@gmail.com>
2019-01-22 15:43:07 -0700

    Only Admin can add other Admin

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,8 +1,23 @@
+// add admin cloud function
+const adminForm = document.querySelector('.admin-actions');
+adminForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const adminEmail = document.querySelector('#admin-email').value;
+    const addAdminRole = functions.httpsCallable('addAdminRole');
+    addAdminRole({ email: adminEmail }).then(result => {
+        console.log(result);
+    })
+})
+
 
 //Listen for auth changes
 auth.onAuthStateChanged(user => {
     console.log(user)
     if (user) {
+        user.getIdTokenResult().then(idTokenResult => {
+            user.admin = idTokenResult.claims.admin;
+        })
         db.collection('guides').onSnapshot(snapshot => {
             setupGuides(snapshot.docs)
             setupUI(user);
@@ -15,6 +30,7 @@ auth.onAuthStateChanged(user => {
         setupGuides([])
         console.log('user logged out')
     }
+
 })
 
 //create new guide
--- scripts/index.js
+++ scripts/index.js
@@ -3,15 +3,20 @@ const guideList = document.querySelector('.guides');
 const loggedOutLinks = document.querySelectorAll('.logged-out')
 const loggedInLinks = document.querySelectorAll('.logged-in')
 const accountDetails = document.querySelector('.account-details')
+const adminItems = document.querySelectorAll('.admin')
 
 const setupUI = (user) => {
     if (user) {
+        if (user.admin) {
+            adminItems.forEach(item => item.style.display = 'block')
+        }
         // account info
         db.collection('users').doc(user.uid).get().then(doc => {
             const html = `
             <div>Name: ${doc.data().fullName}</div>
         <div>Email: ${user.email}</div>
         <div>Bio: ${doc.data().bio}</div>
+        <div class="pink-text">${user.admin ? 'Admin' : ""}</div>
         `;
             accountDetails.innerHTML = html;
         })
@@ -20,6 +25,7 @@ const setupUI = (user) => {
         loggedInLinks.forEach(item => item.style.display = 'block');
         loggedOutLinks.forEach(item => item.style.display = 'none');
     } else {
+        adminItems.forEach(item => item.style.display = 'none')
         // hide account info
         accountDetails.innerHTML = ""
         //toggle UI elements
